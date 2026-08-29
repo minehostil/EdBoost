4a5e36b3c96014c6e0dd48038b64a7bb30c6df4e
Minehostil <108409110+minehostil@users.noreply.github.com>
2026-08-29 05:11:19 -0600

    Actualizar MessageManager.java

--- src/main/java/com/minehostil/edboost/config/MessageManager.java
+++ src/main/java/com/minehostil/edboost/config/MessageManager.java
@@ -34,7 +34,7 @@ public class MessageManager {
             plugin.saveResource("messages.yml", false);
         }
         messages = YamlConfiguration.loadConfiguration(file);
-        prefix = messages.getString("prefix", "7ff55[EdBoost]&r ");
+        prefix = messages.getString("prefix", "&#7ff555[EdBoost]&r ");
     }
 
     /** Envía un mensaje ya formateado (con prefijo, color y placeholders resueltos). */
